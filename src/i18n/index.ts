f95f964eafeb30d0f1bed23405b23980568ec150
simonxue <xueyao.me@gmail.com>
2025-03-02 03:59:31 +0800

    ...

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,7 +4,7 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 // don't want to use this?
-// have a look at the Quick start guide 
+// have a look at the Quick start guide
 // for passing in lng and translations on init
 
 i18n
@@ -23,9 +23,17 @@ i18n
     fallbackLng: 'en-US',
     debug: true,
 
+    // 默认命名空间
+    defaultNS: 'translation',
+    // 插值选项，React 会自动转义，所以这里设为 false
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
-    }
+      escapeValue: false,
+    },
+    // 后端配置，指定翻译文件的加载路径
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+    
   });
 
 
