9088cc904e49c993727964388d3f1c368e193256
simonxue <xueyao.me@gmail.com>
2025-03-02 06:15:39 +0800

    默认中文

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -20,7 +20,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: 'en-US',
+    fallbackLng: 'zh-CN',
     debug: true,
 
     // 默认命名空间
